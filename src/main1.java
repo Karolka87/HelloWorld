8a2020c874581dd8f36126e95c7335ad4e43a64f
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-11-02 20:35:41 +0100

    update1

---
+++ src/main1.java
@@ -0,0 +1,57 @@
+import java.util.concurrent.ThreadLocalRandom;
+
+
+public class main1 {
+
+    public static String randomStringFromArr() {
+        final String p = "papier";
+        final String k = "kamień";
+        final String n = "nożyczki";
+        String[] pkn = {p, k, n};
+
+        int randIdx = ThreadLocalRandom.current().nextInt(pkn.length);
+        String randomElem = pkn[randIdx];
+        return randomElem;
+
+    }
+
+    public static void main(String[] args) {
+
+        String gracz1, gracz2;
+        gracz1 = "Maciej";
+        gracz2 = "Piotr";
+        String x, y;
+        x = randomStringFromArr();
+        y = randomStringFromArr();
+        System.out.println("Imiona dzisiejszych graczy to: " + gracz1 + " oraz " + gracz2);
+
+        for (int i = 0; i <= 19; i++) {
+
+            System.out.println(gracz1 + " wylosował: " + x);
+            System.out.println(gracz2 + " wylosował " + y);
+
+        }
+
+        if (x == "nożyczki" && y == "nożyczki"){
+            System.out.println("Gra zakończyła się remisem");
+        } else if (x == "nożyczki" && y == "papier") {
+            System.out.println("Wygrał " + gracz1);
+        } else if (x == "nożyczki" && y == "kamień") {
+            System.out.println("Wygrał " + gracz2);
+        } else if (x == "papier" && y == "nożyczki") {
+            System.out.println("Wygrał " + gracz2);
+        } else if (x == "papier" && y == "papier") {
+            System.out.println("Gra zakończyła się remisem");
+        } else if (x == "papier" && y == "kamień") {
+            System.out.println("Wygrał " + gracz1);
+        } else if (x == "kamień" && y == "nożyczki") {
+            System.out.println("Wygrał " + gracz1);
+        } else if (x == "kamień" && y == "papier") {
+            System.out.println("Wygrał " + gracz2);
+        } else if (x == "kamień" && y == "kamień") {
+            System.out.println("Gra zakończyła się remisem");
+        }
+
+    }
+}
+
