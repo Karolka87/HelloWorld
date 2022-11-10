c84c2c4812b7279f414786dc21d94be26bd2bc88
Karolka87 <115976221+Karolka87@users.noreply.github.com>
2022-11-10 10:03:54 +0100

    bez podliczania jeszcze

--- src/main1.java
+++ src/main1.java
@@ -1,61 +1,67 @@
 import java.util.concurrent.ThreadLocalRandom;
 
-        public class main1 {
+public class main1 {
 
-            public static final String PAPIER = "papier";
-            public static final String NOZYCZKI = "nożyczki";
-            public static final String KAMIEN = "kamień";
+    public static final String PAPIER = "papier";
+    public static final String NOZYCZKI = "nozyczki";
+    public static final String KAMIEN = "kamien";
 
-            public static final String player1Choice = randomStringFromArr();
-            public static final String player2Choice = randomStringFromArr();
-            public static final String gracz1 = "Maciej", gracz2 = "Piotr";
 
-            public static String randomStringFromArr() {
-                String[] pkn = {PAPIER, KAMIEN, NOZYCZKI};
-                int randIdx = ThreadLocalRandom.current().nextInt(pkn.length);
-                String randomElem = pkn[randIdx];
-                return randomElem;
+    public static final String gracz1 = "Maciej", gracz2 = "Piotr";
 
+    public static String randomStringFromArr() {
+        String[] pkn = {PAPIER, KAMIEN, NOZYCZKI};
+        int randIdx = ThreadLocalRandom.current().nextInt(pkn.length);
+        String randomElem = pkn[randIdx];
+        return randomElem;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Imiona dzisiejszych graczy to: " + gracz1 + " oraz " + gracz2);
+        int player1Result = 0;
+        int player2Result = 0;
+
+
+        for (int i = 0; i <= 19; i++) {
+            String player1Choice = randomStringFromArr();
+            String player2Choice = randomStringFromArr();
+            System.out.println(gracz1 + " wylosowal: " + player1Choice);
+            System.out.println(gracz2 + " wylosowal: " + player2Choice);
+            int result = gameResult(player1Choice, player2Choice);
+
+            if (result == 1) {
+                player1Result++;
             }
-
-            public static void main(String[] args) {
-                //gracz1 = "Maciej";
-                //gracz2 = "Piotr";
-                //String player1Choice, player2Choice;
-                int result = gameResult(player1Choice, player2Choice);
-                System.out.println("Imiona dzisiejszych graczy to: " + gracz1 + " oraz " + gracz2);
-
-
-
+            else if (result == 2) {
+                player2Result++;
             }
-            private static int gameResult(String player1Choice, String player2Choice) {
-                int licznik1 = 0;
-                int licznik2 = 0;
-                System.out.println(gracz1 + " wylosował: " + player1Choice);
-                System.out.println(gracz2 + " wylosował " + player2Choice);
-
-                for (int i = 0; i <= 19; i++) {
-
-
-                    if (player1Choice.equals(player2Choice)){
-                        return 0;
-                    } else if ((player1Choice.equals(NOZYCZKI) && player2Choice.equals(PAPIER)) {
-                        return 1;
-                    } else if ((player1Choice.equals(NOZYCZKI) && (player2Choice.equals(KAMIEN)) {
-                        return 2;
-                    } else if ((player1Choice.equals(PAPIER) && (player2Choice.equals(NOZYCZKI)) {
-                        return 2;
-                    } else if ((player1Choice.equals(PAPIER) && (player2Choice.equals(KAMIEN)) {
-                        return 1;
-                    } else if ((player1Choice.equals(KAMIEN) && (player2Choice.equals(NOZYCZKI)) {
-                        return 1;
-                    } else if ((player1Choice.equals(KAMIEN) && (player2Choice.equals(PAPIER) {
-                        return 2;
-                    }
-                }
-
-                System.out.println("Gracz " + gracz1 + " wygrał: " + licznik1 + " razy");
-                System.out.println("Gracz " + gracz2 + " wygrał: " + licznik2 + " razy");
+            else if (result == 0) {
             }
         }
+
+    }
+
+    private static int gameResult(String player1Choice, String player2Choice) {
+
+
+
+        if (player1Choice.equals(player2Choice)) {
+            return 0;
+        } else if (player1Choice.equals(NOZYCZKI) && player2Choice.equals(PAPIER)) {
+            return 1;
+        } else if (player1Choice.equals(NOZYCZKI) && player2Choice.equals(KAMIEN)) {
+            return 2;
+        } else if (player1Choice.equals(PAPIER) && player2Choice.equals(NOZYCZKI)) {
+            return 2;
+        } else if (player1Choice.equals(PAPIER) && player2Choice.equals(KAMIEN)) {
+            return 1;
+        } else if (player1Choice.equals(KAMIEN) && player2Choice.equals(NOZYCZKI)) {
+            return 1;
+        } else if (player1Choice.equals(KAMIEN) && player2Choice.equals(PAPIER)) {
+            return 2;
+        }
+
+        return -1;
+    }
 }
